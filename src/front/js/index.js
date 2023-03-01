aa4203a30432f415ebbf81300cb01ae488f696a3
majg1987 <majg1987@hotmail.com>
2023-03-01 18:47:10 +0000

    bootstrap imported

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,7 +1,8 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
@@ -9,4 +10,4 @@ import "../styles/index.css";
 import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+ReactDOM.render( < Layout / > , document.querySelector("#app"));
