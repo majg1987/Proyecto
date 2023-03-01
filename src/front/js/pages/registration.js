7f3cf06761066a3fb5203b853a20273775c0361d
majg1987 <majg1987@hotmail.com>
2023-03-01 19:17:00 +0000

    registration page created

---
+++ src/front/js/pages/registration.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { UserRegister } from "../component/Registration/UserRegister.jsx";
+
+export const Registration = () => {
+  return (
+    <div>
+      <UserRegister />
+    </div>
+  );
+};
