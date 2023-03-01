2555851f32c85684f66bee20a32b65cffdc2d430
majg1987 <majg1987@hotmail.com>
2023-03-01 19:17:25 +0000

    login page created

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoginComponent } from "../component/Login/LoginComponent.jsx";
+
+export const Login = () => {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  );
+};
