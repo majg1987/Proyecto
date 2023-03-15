dbf59964bf22d373028f2730fec651262bc2d233
majg1987 <majg1987@hotmail.com>
2023-03-15 17:59:31 +0000

    cover created

---
+++ src/front/js/component/nav/Portada.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../../styles/nav.css";
+
+export const Portada = () => {
+  return (
+    <div className="portada d-flex justify-content-center align-items-center ">
+      <div className="titulo-portada text-center">
+        <p className="mx-4 mb-0">Los Juguetes que no quieras...</p>
+        <p className="mt-0">
+          <span>¡Vendelos!</span>
+        </p>
+      </div>
+    </div>
+  );
+};
