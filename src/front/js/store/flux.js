38f1c2edca65a4ab8cd4eae1c37855b36011c9ac
majg1987 <majg1987@hotmail.com>
2023-03-15 19:27:16 +0000

    user register created in flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,58 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            user: {},
+            registered: false,
+            errorRegister: false,
+        },
+        actions: {
+            // Control de errores
+            errorRegister: () => {
+                setStore({
+                    errorRegister: false
+                })
+            },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+            // User register
+            register: async (user) => {
+                const options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        email: user.email,
+                        password: user.password
+                    })
+                };
+                try {
+                    const response = await fetch(
+                        `${process.env.BACKEND_URL}/api/userRegister`,
+                        options
+                    );
+                    if (response.status === 200) {
+                        setStore({
+                            registered: true
+                        })
+                    }
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+                    setStore({
+                        registered: false
+                    })
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                } catch (error) {
+                    console.log(`Error: ${error}`);
+                    setStore({
+                        errorRegister: true
+                    })
+                }
+            }
+        }
+    };
 };
 
 export default getState;
